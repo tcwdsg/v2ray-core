923954e6ccf366b9fde8d25e37692056d951e286
x <x@gmail.com>
2020-04-19 11:07:34 +0800

    debug

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -7,4 +7,4 @@ cd $basedir/infra/control/main
 env CGO_ENABLED=0 go build -o $basedir/bin/v2ctl -tags confonly -ldflags "-s -w"
 
 cd $basedir
-sh $basedir/bin/v2ray -config $basedir/bin/v2ray_test.json
+$basedir/bin/v2ray -config $basedir/bin/v2ray_test.json
