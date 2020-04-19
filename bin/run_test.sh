d42b8540f064e574c61be29614898a4b8375f2df
x <x@gmail.com>
2020-04-19 11:04:41 +0800

    debug

---
+++ bin/run_test.sh
@@ -0,0 +1,10 @@
+
+basedir=/data/gs/v2ray-core
+cd $basedir/main
+env CGO_ENABLED=0 go build -o $basedir/bin/v2ray -ldflags "-s -w"
+
+cd $basedir/infra/control/main
+env CGO_ENABLED=0 go build -o $basedir/v2ctl -tags confonly -ldflags "-s -w"
+
+cd $basedir
+sh $basedir/v2ray -config $basedir/bin/v2ray_test.json
